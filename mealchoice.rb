90383f2e058e655108688d9e2b050583ca346e63
christina15339 <clamberg333@gmail.com>
2018-07-24 15:41:01 +0000

    automatically backed up by learn

--- mealchoice.rb
+++ mealchoice.rb
@@ -1,14 +1,14 @@
-def food (breakfeast = "frosted flacks") 
+def food (frosted flacks) 
   puts "i love having #{breakfeast} in the morning"
 end
-def food("frosted flakes")
+def food(frosted flakes)
  
- def foods(lunch = "cheese")
+ def foods(lunch = cheese)
   puts "i love having #{lunch} at noon"
 end
 def foods("grilled cheese")
   
-def morefood ("steak")
+def morefood (steak)
   puts "i love having #{dinner} at six"
 end 
 def morefood ("salmon")
