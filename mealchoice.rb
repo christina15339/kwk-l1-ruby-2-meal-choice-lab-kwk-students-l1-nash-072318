fd15474698122be47843f94b600bdcf9ce6791f9
christina15339 <clamberg333@gmail.com>
2018-07-24 15:34:07 +0000

    automatically backed up by learn

--- mealchoice.rb
+++ mealchoice.rb
@@ -1,7 +1,7 @@
 def food (breackfeast = "frosted flacks", lunch = "grilled cheese", dinner = "salmon")
-  puts "i love having breackfast in the morning"
-  puts "i love having Lunch at noon"
-  puts "i love having dinner at six"
+  puts "i love having #{breackfast} in the morning"
+  puts "i love having #{lunch} at noon"
+  puts "i love having #{dinner} at six"
 end 
 
 def food("toast","burgers","steak")
