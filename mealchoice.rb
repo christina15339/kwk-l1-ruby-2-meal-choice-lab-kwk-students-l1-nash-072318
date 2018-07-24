15b181056e1517474c3d4a468f633abbceec10b3
christina15339 <clamberg333@gmail.com>
2018-07-24 15:32:49 +0000

    automatically backed up by learn

--- mealchoice.rb
+++ mealchoice.rb
@@ -1,4 +1,4 @@
-def food (breackfeast = frosted flacks, lunch = grilled cheese, dinner = salmon)
+def food (breackfeast = "frosted flacks", lunch = "grilled cheese", dinner = "salmon")
   puts "i love having breackfast in the morning"
   puts "i love having Lunch at noon"
   puts "i love having dinner at six"
